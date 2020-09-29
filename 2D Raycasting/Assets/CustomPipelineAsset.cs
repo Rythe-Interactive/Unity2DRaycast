d0610ca76b79da2f12834b0368d180d7ea1053bb
kukash <469260@student.saxion.l>
2020-09-29 10:45:17 +0200

    Imported SRP

---
+++ 2D Raycasting/Assets/CustomPipelineAsset.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UnityEngine.Rendering
+{
+    [CreateAssetMenu]
+    public class CustomPipelineAsset : RenderPipelineAsset
+    {
+        protected override RenderPipeline CreatePipeline()
+        {
+
+            return null;
+        }
+    }
+
+}
